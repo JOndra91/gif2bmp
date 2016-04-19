38aac55066d723bc9ee0547ac285e2ff5857b722
Ondřej Janošík <j.ondra14@gmail.com>
2016-04-20 00:19:49 +0200

    Image descriptor

---
+++ src/imageDescriptor.cpp
@@ -0,0 +1,32 @@
+/**
+ * File: imageDescriptor.cpp
+ * Author: Ondřej Janošík <xjanos12@stud.fit.vutbr.cz>
+ * Created: 2016/04/20
+ */
+
+#include "imageDescriptor.hpp"
+#include "blocks.hpp"
+
+#include <cassert>
+
+using namespace gif;
+
+ImageDescriptor::ImageDescriptor(IReader *reader) {
+  ImageDescriptorPackedField packedField;
+  reader->allocate(10);
+
+  // TODO: Proper handling
+  assert(reader->readByte() == (unsigned)Block::ImageSeparator);
+
+  m_left = reader->readWord();
+  m_right = reader->readWord();
+  m_width = reader->readWord();
+  m_height = reader->readWord();
+
+  packedField.packedValue = reader->readByte();
+
+  m_colorTableFlag = packedField.colorTableFlag;
+  m_interlaceFlag = packedField.interlaceFlag;
+  m_sortFlag = packedField.sortFlag;
+  m_colorTableSize = packedField.colorTableSize;
+}
---
+++ src/imageDescriptor.hpp
@@ -0,0 +1,75 @@
+/**
+ * File: imageDescriptor.hpp
+ * Author: Ondřej Janošík <xjanos12@stud.fit.vutbr.cz>
+ * Created: 2016/04/20
+ */
+
+#pragma once
+
+#include <cstdint>
+#include "iReader.hpp"
+
+namespace gif {
+
+  union ImageDescriptorPackedField {
+    struct {
+      bool colorTableFlag : 1;
+      bool interlaceFlag : 1;
+      bool sortFlag : 1;
+      int reserved : 2;
+      unsigned colorTableSize : 3;
+    };
+    uint8_t packedValue;
+  };
+
+  class ImageDescriptor {
+
+    unsigned m_left;
+    unsigned m_right;
+    unsigned m_width;
+    unsigned m_height;
+
+    bool m_colorTableFlag;
+    bool m_interlaceFlag;
+    bool m_sortFlag;
+    unsigned m_colorTableSize;
+
+  public:
+
+    ImageDescriptor(IReader *reader);
+
+    inline unsigned getLeft() {
+      return m_left;
+    };
+
+    inline unsigned getRight() {
+      return m_right;
+    };
+
+    inline unsigned getWidth() {
+      return m_width;
+    };
+
+    inline unsigned getHeight() {
+      return m_height;
+    };
+
+    inline bool hasColorTable() {
+      return m_colorTableFlag;
+    };
+
+    inline bool isInterlaced() {
+      return m_interlaceFlag;
+    };
+
+    inline bool isisColorTableOrdered() {
+      return m_sortFlag;
+    };
+
+    inline unsigned getColorTableSize() {
+      return m_colorTableSize;
+    };
+
+  };
+
+}
